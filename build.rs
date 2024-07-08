ec564ca938b4b0904cc5d1a84cbc0f4f605eff75
Emmanuel Coste <emmanuel.coste@cosmian.com>
2024-07-08 10:26:18 +0200

    ci: test windows build

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::{process, time::Duration};
 
 use serde::Deserialize;
 
@@ -7,18 +7,26 @@ struct Release {
     tag_name: String,
 }
 
-fn get_latest_kms_release(client: &reqwest::blocking::Client) -> String {
+fn get_latest_kms_release(client: &reqwest::blocking::Client) -> Option<String> {
     std::env::var("VERSION")
         .ok()
         .filter(|version| !version.is_empty())
-        .unwrap_or_else(|| {
-            client
-                .get("https://api.github.com/repos/Cosmian/kms/releases/latest")
-                .send()
-                .unwrap()
-                .json::<Release>()
-                .unwrap()
-                .tag_name
+        .or_else(|| {
+            for _ in 0..5 {
+                if let Ok(response) = client
+                    .get("https://api.github.com/repos/Cosmian/kms/releases/latest")
+                    .send()
+                {
+                    if let Ok(release) = response.json::<Release>() {
+                        if !release.tag_name.is_empty() {
+                            return Some(release.tag_name);
+                        }
+                    }
+                }
+                // Wait for a short period before retrying
+                std::thread::sleep(Duration::from_secs(2));
+            }
+            None
         })
 }
 
@@ -31,10 +39,11 @@ fn main() {
     // typically the `VERSION` env var is set by the KMS CI release pipeline
     let latest_kms_release = get_latest_kms_release(&client);
 
-    if latest_kms_release.is_empty() {
+    let latest_kms_release = latest_kms_release.unwrap_or_else(|| {
         println!("Latest KMS release version cannot be empty");
         process::exit(1);
-    }
+    });
+
     println!("cargo:warning=Using KMS release {latest_kms_release}");
 
     // get `ckms` `main.rs` file
