ce8881fc5abb2493a52f0890f1e68ff177ce88ae
Manuthor <manu.coste@gmail.com>
2024-10-09 07:02:34 +0200

    ci: add script to check openssl link

--- .github/scripts/check_openssl.sh
+++ .github/scripts/check_openssl.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-set -ex
+set -x
 
 if [ "$(uname)" = "Linux" ]; then
-  ldd "$1" | grep ssl && exit 1
+  ldd "$1"
+  check=$(ldd "$1" | grep ssl)
+  if [ -n "$check" ]; then
+    echo "Found dynamic Openssl link"
+    exit 1
+  fi
 else
-  otool -L "$1" | grep openssl && exit 1
+  otool -L "$1"
+  check=$(otool -L "$1" | grep openssl)
+  if [ -n "$check" ]; then
+    echo "Found dynamic Openssl link"
+    exit 1
+  fi
 fi
